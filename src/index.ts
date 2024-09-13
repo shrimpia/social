84cdb1b63d5784891fd1f0b88598d67bdb84c60d
Ebise Lutica <lutica@shrimpia.network>
2024-09-13 16:35:44 +0000

    本番環境のビルドに対応

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
+import Fastify from 'fastify';
 import FastifyView from '@fastify/view';
+import FastifyStatic from '@fastify/static';
+import path from 'node:path';
+import fs from 'node:fs';
+import ws from '@fastify/websocket';
 import { fastifyTRPCPlugin, type FastifyTRPCPluginOptions } from '@trpc/server/adapters/fastify';
 import ejs from 'ejs';
-import Fastify from 'fastify';
+
 import { appRouter, type AppRouter } from './api/router';
 import { createApiContext } from './api/context';
-import ws from '@fastify/websocket';
 import { TITLE } from './client/const';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const app = Fastify();
 
 app.register(ws);
+app.register(FastifyStatic, {
+  root: path.join(import.meta.dirname, '../dist/assets'),
+  prefix: '/assets/', // 静的ファイルのパス
+});
 
 app.register(FastifyView, {
   engine: { ejs },
@@ -36,10 +42,76 @@ app.register(fastifyTRPCPlugin, {
   } satisfies FastifyTRPCPluginOptions<AppRouter>['trpcOptions'],
 });
 
-// フロントエンド。基本的に全エンドポイントに対して同じHTMLを返す
-app.get('/*', async (request, reply) => {
-  return reply.viewAsync('index.ejs', { title: TITLE, url: process.env.FRONTEND_URL });
-});
+// フロントエンド。
+if (process.env.NODE_ENV !== 'production') {
+  // 開発時はViteの開発サーバーを利用
+  app.get('/*', async (request, reply) => {
+    return reply.viewAsync('index.ejs', { title: TITLE });
+  });
+} else {
+  // 本番環境ではViteのビルド結果を利用
+  // manifest.jsonを読み込む
+  const manifestPath = path.join(import.meta.dirname, '../dist', '.vite', 'manifest.json');
+  let manifest: Record<string, any> = {};
+
+  try {
+    const data = fs.readFileSync(manifestPath, 'utf-8');
+    manifest = JSON.parse(data);
+  } catch (err) {
+    console.error('フロントエンドのビルドデータが見つかりません。pnpm run build を実行してからもう一度開始してください！');
+    process.exit(1);
+  }
+
+  // エントリポイントを探す
+  const entrypoint = Object.values(manifest).find(v => v.isEntry);
+
+  const modules: string[] = [];
+  const styles: string[] = [];
+  const preloadedModules: string[] = [];
+
+  // エントリーポイントのcssを全て読み込む
+  if (entrypoint.css) {
+    styles.push(...entrypoint.css);
+  }
+
+  // 依存するモジュールのcssを全て読み込む
+  if (entrypoint.imports) {
+    for (const imp of entrypoint.imports) {
+      if (imp.css) {
+        styles.push(...imp.css);
+      }
+    }
+  }
+
+  // エントリポイントのモジュールを全て読み込む
+  if (entrypoint.file) {
+    modules.push(entrypoint.file);
+  }
+
+  // 依存するモジュールをmodulepreloadとして読み込む
+  if (entrypoint.imports) {
+    for (const imp of entrypoint.imports) {
+      if (imp.file) {
+        preloadedModules.push(imp.file);
+      }
+    }
+  }
+  if (entrypoint.dynamicImports) {
+    for (const imp of entrypoint.dynamicImports) {
+      if (imp.file) {
+        preloadedModules.push(imp.file);
+      }
+    }
+  }
+  app.get('/*', async (request, reply) => {
+    return reply.viewAsync('index.ejs', {
+      title: TITLE,
+      styles,
+      modules,
+      preloadedModules,
+    });
+  });
+}
 
 app.listen({ host: '0.0.0.0', port: 3000 }, (err) => {
   if (err) {
@@ -49,9 +121,9 @@ app.listen({ host: '0.0.0.0', port: 3000 }, (err) => {
 
   const info = app.server.address();
   if (typeof info !== 'object') {
-    console.log(`Server is ready: ${info}`);
+    console.info(`Server is ready: ${info}`);
     return;
   }
 
-  console.log(`Server is ready: http://${info?.address}:${info?.port}`);
+  console.info(`Server is ready: http://${info?.address}:${info?.port}`);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import react from '@vitejs/plugin-react';
 import pages from 'vite-plugin-pages';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [
@@ -18,7 +15,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
-    origin: process.env.FRONTEND_URL,
+    origin: 'http://localhost:5173',
   },
   build: {
     manifest: true,
